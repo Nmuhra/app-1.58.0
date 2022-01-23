ce728b01c683cd2c92cdb8d7ea6d77e6dcd9f241
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-01-23 21:16:41 +0200

    Add files via upload
    find how much you weigh on different planets

---
+++ planetary-weight.py
@@ -0,0 +1,17 @@
+# let's build a program to measure our weight on different
+
+#  planets and moons in our solar system
+
+print("Hi, are you planing to travel to different a different planet, well you should probably know your how much you weigh there first")
+
+weight = input("what is your weight in kgs?\n")
+
+Planets = "mercury, venus, mars, jupiter, saturn "
+
+print("Which planet are you traveling to? Here are the options: \n" + Planets)
+
+choice = input()
+
+
+
+

295a00a5bb394a0dba8b9f0e83ba23c50bcc370b
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-01-24 20:13:31 +0200

    Update planetary-weight.py
    A program made to let measure your weight on different planets in our solar system.

--- planetary-weight.py
+++ planetary-weight.py
@@ -1,10 +1,11 @@
 # let's build a program to measure our weight on different
 
 #  planets and moons in our solar system
+name= input("Name?\n")
 
-print("Hi, are you planing to travel to different a different planet, well you should probably know your how much you weigh there first")
+print("okay " + name + ", are you planing to travel to a different planet, well you should probably know your how much you weigh there first")
 
-weight = input("what is your weight in kgs?\n")
+weight = int(input("what is your weight in kgs?\n"))
 
 Planets = "mercury, venus, mars, jupiter, saturn "
 
@@ -12,6 +13,34 @@ print("Which planet are you traveling to? Here are the options: \n" + Planets)
 
 choice = input()
 
+mercury_weight= round((weight/9.81)*3.7)
 
+venus_weight= round((weight/9.81)*8.87)
 
+mars_weight= round((weight/9.81)*3.711)
 
+jupiter_weight= round((weight/9.81)*24.79)
+
+saturn_weight= round((weight/9.81)*10.44)
+
+if choice == "mercury":
+ 
+    print(f"your weight on  {choice} is {mercury_weight}")
+
+if choice == "venus":
+ 
+    print(f"your weight on  {choice} is {venus_weight}")
+
+if choice == "mars":
+ 
+    print(f"your weight on  {choice} is {mars_weight}")
+
+if choice == "jupiter":
+ 
+    print(f"your weight on  {choice} is {jupiter_weight}") 
+
+if choice == "saturn":
+ 
+    print(f"your weight on  {choice} is {saturn_weight}")  
+
+print("cool " + name + ", we are preparing your rocket to " + choice +" , pack up your things we are leaving soon")
